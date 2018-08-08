25af33d5c08adb99952788ed5e475814397186f6
Ben Robinson <mentionbenjamin@gmail.com>
2018-08-08 09:49:07 +0100

    adds list for each instrument in their respective family

--- src/views/instrument_detail_view.js
+++ src/views/instrument_detail_view.js
@@ -14,11 +14,23 @@ InstrumentDetailView.prototype.bindEvents = function () {
 InstrumentDetailView.prototype.render = function(instrumentFamily) {
   const header = document.createElement('h1');
   const paragraph = document.createElement('p');
+  const listContainer = document.createElement('ul');
   header.textContent = `The family is ${instrumentFamily.name}`;
-  paragraph.textContent = `${instrumentFamily.description} ${instrumentFamily.instruments}`;
+  paragraph.textContent = `${instrumentFamily.description}`;
+  list.textContent = `${instrumentFamily.instruments}`;
   this.container.innerHTML = '';
   this.container.appendChild(header);
   this.container.appendChild(paragraph);
+  this.container.appendChild(listContainer);
+
+  instrumentFamily.instruments.forEach(element => {
+    const instrumentItem = document.createElement('li');
+    instrumentItem.textContent = element;
+    listContainer.appendChild(instrumentItem);
+  });
 };
+
+const list = document.createElement('ul');
+
 
 module.exports = InstrumentDetailView;
